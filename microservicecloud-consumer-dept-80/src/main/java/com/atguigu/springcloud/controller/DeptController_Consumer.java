23396cdd3db1cf181eb1a7eecaa739153bdf0295
htdingkang <ht_dingkang@126.com>
2019-09-01 17:16:13 +0800

    ribbon 负载均衡

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/controller/DeptController_Consumer.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/controller/DeptController_Consumer.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.Dept;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,10 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 @RestController
+@EnableEurekaClient
 public class DeptController_Consumer {
-    public static final String REST_URL_PREFIX="http://192.168.1.123:8001";
+    //public static final String REST_URL_PREFIX="http://192.168.1.123:8001";
+    public static final String REST_URL_PREFIX="http://microservicecloud-dept";
     @Autowired
     RestTemplate restTemplate;
     @RequestMapping("/consumer/dept/add")
